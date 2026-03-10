817135458d721c9494e114549df1453908d7ccd7
Joel Scholl <joel@scholls.ch>
2026-03-10 18:38:28 +0100

    wip: add ripgrep alias to print task due today

--- init.lua
+++ init.lua
@@ -190,6 +190,12 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('n', '<leader>ot', function()
+  local date = os.date '%Y-%m-%d'
+  local vault = vim.fn.expand '/mnt/c/Users/SchollJ/Documents/notes/obsidian/' -- change this
+  vim.cmd 'new'
+  vim.fn.termopen("rg -e '- \\[ \\].*" .. date .. "' " .. vault .. ' --no-heading --no-filename --no-line-number')
+end, { desc = 'Obsidian tasks due today' })
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
