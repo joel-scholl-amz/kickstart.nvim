086b6d8b312ffa36bb26a4c3258693f705c9feee
Joel Scholl <joel@Jols-Gaming-PC.localdomain>
2026-03-29 17:44:50 +0200

    wip: add kubectl nvim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -94,4 +94,16 @@ return {
       require('claude-code').setup()
     end,
   },
+  {
+    'Ramilito/kubectl.nvim',
+    opts = {},
+    cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
+    keys = {
+      { '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>' },
+      { '<C-k>', '<Plug>(kubectl.kill)', ft = 'k8s_*' },
+      { '7', '<Plug>(kubectl.view_nodes)', ft = 'k8s_*' },
+      { '8', '<Plug>(kubectl.view_overview)', ft = 'k8s_*' },
+      { '<C-t>', '<Plug>(kubectl.view_top)', ft = 'k8s_*' },
+    },
+  },
 }
